a6929af53b80ced2761d3abcba62897bb1f68f19
Chris Heppell <clheppell@icloud.com>
2019-08-31 14:58:22 +0100

    Revert "Chnages to payment emails and code to show by type (in cols) in CSV export"

--- src/controllers/payments/admin/history/FinanceReport.json.php
+++ src/controllers/payments/admin/history/FinanceReport.json.php
@@ -24,7 +24,6 @@ $getPayments->execute(['searchDate' => $searchDate]);
 $now = new DateTime('now', new DateTimeZone('UTC'));
 
 $array = [];
-$types = [];
 
 // output the column headings
 //$array += ['about' => env('CLUB_NAME') . ' Finance Report'];
@@ -47,20 +46,6 @@ while ($row = $getPayments->fetch(PDO::FETCH_ASSOC)) {
     }
     $details = $row['User'] . ', ' . $name . ' ' . $infoText;
     $status = paymentStatusString($row['Status']);
-    $grouping = null;
-    if (isset($json->type)) {
-      $grouping = [
-        'object' => $json->type->object,
-        'id' => $json->type->id,
-        'name' => $json->type->name
-      ];
-      if (!isset($types[$json->type->object])) {
-        $types += [$json->type->object => []];
-      }
-      if (!isset($types[$json->type->object][$json->type->id])) {
-        $types[$json->type->object] += [$json->type->id => $json->type->name];
-      }
-    }
     $item = [
       'object' => 'Payment',
       'type' => $row['DebitCredit'],
@@ -69,8 +54,7 @@ while ($row = $getPayments->fetch(PDO::FETCH_ASSOC)) {
       'credits' => (int) $in,
       'debits' => (int) $out,
       'income' => 'Gross',
-      'status' => $status,
-      'grouping' => $grouping
+      'status' => $status
     ];
     $array[] = $item;
   } else if ($row['Type'] == 'Payouts') {
@@ -124,6 +108,5 @@ $output = [
   'month' => $month,
   'year' => $year,
   'date_produced' => $now->format("Y-m-d"),
-  'types' => $types,
   'items' => $array,
 ];
