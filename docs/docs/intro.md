81026f77d978fded966f6fd34bc282d0ae7184c0
Chris Heppell <chris.heppell@myswimmingclub.uk>
2022-02-14 19:58:51 +0000

    Start docusaurus work

---
+++ docs/docs/intro.md
@@ -0,0 +1,23 @@
+---
+sidebar_position: 1
+---
+
+# Docs
+
+Welcome to the Help and Support Documentation for the Membership System by Swimming Club Data Systems (SCDS).
+
+## How to get more help
+
+### Parents
+
+Always speak to an official from your own club first. They will be able to access additional help and support from SCDS as required.
+
+In order to manage workload, parents are unable to access support from SCDS beyond the documentation provided here and on your club's website.
+
+### Club Officials
+
+If you are unable to resolve your own or a parent's problem, contact SCDS by emailing support@myswimmingclub.co.uk quoting your name, club and position as well as giving as much detail about the problem as possible.
+
+### Self Hosted
+
+If you're a self hosted user, support from SCDS is unavailable. You may wish to speak to your technical manager who will be able to work with [SCDS via GitHub Issues](https://github.com/Chester-le-Street-ASC/Membership).
---
+++ docs/docusaurus.config.js
@@ -0,0 +1,110 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Membership Help and Support',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Swimming-Club-Data-Systems', // Usually your GitHub org/user name.
+  projectName: 'Membership', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Membership Help and Support',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/scds.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'All Documentation',
+          },
+          {
+            href: 'https://myswimmingclub.uk/clubs',
+            label: 'Login',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/Swimming-Club-Data-Systems/Membership',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/myswimmingclub',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Swimming-Club-Data-Systems/Membership',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Swimming Club Data Systems.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
